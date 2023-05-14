c894573e3a5224ed2c0fdf260cf5e2c27bf1d52e
Nardin Philip <78703626+Nardin151@users.noreply.github.com>
2023-05-14 19:51:22 +0300

    Update Question 2.py
    Add another answer to the question using the end time

--- Question 2.py
+++ Question 2.py
@@ -1,12 +1,23 @@
 jobs = [[1, 6, 6], [2, 5, 5],[5, 7, 5], [6, 8, 3]]
+# Using start point:
 jobs.sort(key=lambda x:x[0])
 weights = [row[-1] for row in jobs]
 maxProfit = [row[-1] for row in jobs]
-#using start point
 for i in range(len(jobs)):
     for j in range(i):
         if(i==j):
             continue
         if jobs[j][1] <= jobs[i][0]:
+            maxProfit[i] = max(maxProfit[i], maxProfit[j]+weights[i])
+print(max(maxProfit))
+# Using the end point:
+jobs.sort(key=lambda x:x[1])
+weights = [row[-1] for row in jobs]
+maxProfit = [row[-1] for row in jobs]
+for i in range(len(jobs)-1,-1,-1):
+    for j in range(i,-1,-1):
+        if(i==j):
+            continue
+        if jobs[i][0] >= jobs[j][1]:
             maxProfit[i] = max(maxProfit[i], maxProfit[j]+weights[i])
 print(max(maxProfit))
